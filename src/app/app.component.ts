09debff31d8fd0e6a9d020247b56867e6288d02e
nydi <stefan.nydegger@gmail.com>
2018-09-05 22:58:31 +0200

    lecture 370

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {state, trigger, style} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color' : 'red',
+        'translate': 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        'translate': 'translateX(100px)'
+      }))
+    ])
+
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
